ff66b6578c7b2eb7bb5abf23fe19549fdbf0496a
jasonmag <jasonmaglangit@gmail.com>
2023-10-11 22:58:28 +0800

    added Blog show function with view templates; added flash alert for empty record request

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,11 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
     @blogs = Blog.limit(10) # TODO paginate
+  end
+
+  def show
   end
 
   def create
@@ -18,5 +22,14 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.permit(:title,:image,:body)
+  end
+
+  def set_blog
+    @blog = Blog.find_by(id: params[:id])
+
+    unless @blog
+      flash[:alert] = "Blog not found."
+      redirect_to blogs_path
+    end
   end
 end
